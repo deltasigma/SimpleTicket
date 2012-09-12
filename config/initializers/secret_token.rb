894d33bd41f7adcdf61b230fdb612492a472eb31
Fabio Costa <fabio.costa@deltasigma.com.br>
2012-09-11 22:39:54 -0300

    Initial Commit...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTicket::Application.config.secret_token = '0459a287826cd29dbf63d01d6455a64d4a5f99105fc1303a96416f773d9410eb6b161ff78f44d4e45540949c701755266a3c7d26fb731a20f349d72e55a32f2f'
